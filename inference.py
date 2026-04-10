d89b4d781cf78ef0092d0d24911eee3d669848da
mohit lovanshi <mohit.0832cs241133@cdgi.edu.in>
2026-04-10 11:39:43 +0530

    Remove main entry point for uvicorn server
    Removed the main entry point for running the application.

--- inference.py
+++ inference.py
@@ -39,6 +39,3 @@ def state():
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=7860)
